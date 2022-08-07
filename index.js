22b39a4b444f69a87b70e7b80b7c7f721e27c71e
Jenny Aguilar <contact@jaguilarweb.com>
2022-08-07 13:44:52 +0200

    feat: Add query endpoint for users

--- index.js
+++ index.js
@@ -10,12 +10,54 @@ app.get('/ruta', (req, res) => {
   res.send('Cambiando ruta!');
 });
 
-app.get('/productos', (req, res) => {
-  res.json({
-    name: "Producto 1",
-    price: 100
-  });
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: "Producto 1",
+      price: 100
+    },
+    {
+      name: "Producto 2",
+      price: 200
+    }]);
 });
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: "Producto",
+    price: 300
+  });
+})
+
+//Endpoint con 2 parametros dinamicos en la url
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId} = req.params;
+  res.json({
+    categoryId,
+    productId
+  });
+})
+
+// Parámetros query para hacer filtros
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset
+    });
+  } else {
+    res.send('No hay parametros')
+  }
+
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
