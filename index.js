ba48eb6476d7b6803fcf5c1296186b46e79e9abf
Jenny Aguilar <contact@jaguilarweb.com>
2022-08-07 12:55:35 +0200

    feat: Add routing

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World Express!');
+});
+
+app.get('/ruta', (req, res) => {
+  res.send('Cambiando ruta!');
+});
+
+app.get('/productos', (req, res) => {
+  res.json({
+    name: "Producto 1",
+    price: 100
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
